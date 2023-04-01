a50cd01ab4f2a71fde76c5ac0028043edc803577
Bernardo Branco <bernardomspbranco@gmail.com>
2023-04-01 23:49:49 +0100

    s

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 // import { setActiveSong, playPause } from '../redux/features/playerSlice';
